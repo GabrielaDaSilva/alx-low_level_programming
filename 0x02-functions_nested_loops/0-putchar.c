962204c43c9c7484acd0c740cfe4cb39a715d6b4
GabrielaDaSilva <gabrieladasilva70@gmail.com>
2023-02-21 08:17:08 -0800

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - Prints _putchar, followed by a new line.
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	_puthchar('_');
+	_puthchar('p');
+	_puthchar('u');
+	_puthchar('t');
+	_puthchar('c');
+	_puthchar('h');
+	_puthchar('a');
+	_puthchar('r');
+	_puthchar('\n');
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * main - _putchar writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On sucess 1.
+ * On eror, -1 is returned, errno is set appropriately.
+ */
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
