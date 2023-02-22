fe8f533a5679695b81c52c6ab7fa19ab92c4ef1d
GabrielaDaSilva <gabrieladasilva70@gmail.com>
2023-02-22 08:08:03 -0800

    0-main.c

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * test that the function positive_or_negative()
+ * gives the correct output if given a case of 0.
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i)
+	return (0)
+}
