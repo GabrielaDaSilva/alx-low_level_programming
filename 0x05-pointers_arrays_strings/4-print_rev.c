1765b52f44ebbda9aba47d2654fc75cc3d950a51
GabrielaDaSilva <gabrieladasilva70@gmail.com>
2023-02-27 08:40:00 -0800

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_rev -  prints a string in reverse,
+ * followed by a new line.
+ * @s: string to print
+ * Return: 0.
+ */
+void print_rev(char *s)
+{
+	int stringi;
+	int d;
+
+	while (*s != '\0')
+	{
+		stringi++;
+		s++;
+	}
+	s--;
+	for (d = stringi; d > 0; d--)
+	{
+		_putchar(*s);
+		s--;
+	}
+	_putchar('\n')
+}
